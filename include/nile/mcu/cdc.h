762a621138ced4c944b8893ef451eabb9869acf4
Adrian Siekierka <kontakt@asie.pl>
2025-10-23 22:37:03 +0200

    mcu/cdc: fix functions

--- include/nile/mcu/cdc.h
+++ include/nile/mcu/cdc.h
@@ -25,20 +25,20 @@
 
 #include "../mcu.h"
 
-static inline int16_t nile_mcu_native_cdc_read_sync(void __far* buffer, uint16_t buflen) {
+static inline int16_t nile_mcu_native_cdc_read_sync(void __wf_cram* buffer, uint16_t buflen) {
     int16_t result;
     if ((result = nile_mcu_native_send_cmd(NILE_MCU_NATIVE_CMD(0x40, buflen), NULL, 0)) < 0) return result;
     return nile_mcu_native_recv_cmd(buffer, buflen);
 }
 
-static inline int16_t nile_mcu_native_cdc_write_sync(const void __wf_cram* buffer, uint16_t buflen) {
+static inline int16_t nile_mcu_native_cdc_write_sync(const void __far* buffer, uint16_t buflen) {
     int16_t result, bytes;
     if ((result = nile_mcu_native_send_cmd(NILE_MCU_NATIVE_CMD(0x41, buflen), buffer, buflen)) < 0) return result;
     if ((result = nile_mcu_native_recv_cmd(&bytes, 2)) < 0) return result;
     return bytes;
 }
 
-static inline int16_t nile_mcu_native_cdc_write_async_start(const void __wf_cram* buffer, uint16_t buflen) {
+static inline int16_t nile_mcu_native_cdc_write_async_start(const void __far* buffer, uint16_t buflen) {
     int16_t result;
     if ((result = nile_mcu_native_send_cmd(NILE_MCU_NATIVE_CMD(0x41, buflen), buffer, buflen)) < 0) return result;
     return nile_mcu_native_recv_cmd_start(2);
@@ -52,9 +52,9 @@ static inline int16_t nile_mcu_native_cdc_available_sync(void) {
     return bytes;
 }
 
-static inline int16_t nile_mcu_native_cdc_available_async_start(const void __wf_cram* buffer, uint16_t buflen) {
+static inline int16_t nile_mcu_native_cdc_available_async_start(void) {
     int16_t result;
-    if ((result = nile_mcu_native_send_cmd(NILE_MCU_NATIVE_CMD(0x43, 0), buffer, buflen)) < 0) return result;
+    if ((result = nile_mcu_native_send_cmd(NILE_MCU_NATIVE_CMD(0x43, 0), NULL, 0)) < 0) return result;
     return nile_mcu_native_recv_cmd_start(2);
 }
 #define nile_mcu_native_cdc_available_async_finish nile_mcu_native_recv_cmd_response_int16
